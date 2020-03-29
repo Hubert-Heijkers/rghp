72b7b13b551f89af774371ad5ec975602f226bc6
Hubert Heijkers <hubert.heijkers@nl.ibm.com>
2020-03-29 23:00:49 +0200

    Updates

---
+++ precache-manifest.d9699ecd5c02670c61cdc4253347f062.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcb54fca07f959b92c3dbc7f5b971106",
+    "url": "/rghp/index.html"
+  },
+  {
+    "revision": "9878be02701bf37d6b4a",
+    "url": "/rghp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b7d52bb5b4aa44da25a5",
+    "url": "/rghp/static/js/2.9403dfb0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rghp/static/js/2.9403dfb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9878be02701bf37d6b4a",
+    "url": "/rghp/static/js/main.0f06a8cc.chunk.js"
+  },
+  {
+    "revision": "fd84420543150cf4a9b5",
+    "url": "/rghp/static/js/runtime-main.465aed13.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rghp/static/media/logo.5d5d9eef.svg"
+  }
+]);
